e28b91ce53ac89a15bc01de2b158489552844d87
Stephen-Li-ICD2O1-01 <stephen.li@student.tdsb.on.ca>
2026-05-01 14:56:04 +0000

    save

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,35 @@
+/*
+File: Lesson 5.1 - Introduction to Arrays
+Author: Stephen Li
+Date Created: April 20, 2026
+Date Last Modified: April 27, 2026
+*/
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-
 	}
-
-	public static void q1() {
-		//Write question 1 code here
+	
+	public static int[] createIntArray() {
+	    int[] array = new int[5];
+	    for (int i = 0; i < 5; i++) {
+	        array[i] = i + 1;
+	        System.out.print(array[i] + " ");
+	    }
+	    return array;
 	}
-
-	public static void q2() {
-		//Write question 2 code here
+	
+	public static String[] createArray(String word1, String word2, String word3, String word4) {
+	    String[] array = {word1, word2, word3, word4};
+	    return array;
 	}
-
-	public static void q3() {
-		//Write question 3 code here
+	
+	public static String getElement(int num1, String[] array) {
+	    return array[num1];
 	}
-
-	public static void q4() {
-		//Write question 4 code here
+	
+	public static double[] replaceElement(int num1, double double1, double[] array) {
+	    array[num1] = double1;
+		return array;
 	}
-
-	public static void q5() {
-		//Write question 5 code here
-	}
-
 }
